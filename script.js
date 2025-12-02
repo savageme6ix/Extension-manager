db799d44cb420cd49c2658315cd365fcc3511702
savageme6ix <maingithomas803@gmail.com>
2025-12-02 10:25:22 +0300

    Set grid layout

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const extensionss = fetch("data.json")
-    .then(response=>{
-        if(!response.ok){
-            throw new Error (`Http error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data=>{
-        console.log(data)
-    })    .catch(error=>{
-        console.error('There was a problem', error)
-    })
+// let gridHtml = ``;
+// const extensionss = fetch("data.json")
+//     .then(response=>{
+//         if(!response.ok){
+//             throw new Error (`Http error! status: ${response.status}`);
+//         }
+//         return response.json();
+//     })
+//     .then(data=>{
+//         console.log(data)
+//         data.forEach((extension)=>{
+//             console.log(extension.name)
+//           const main = document.querySelector(".grid-container")
+//           main.innerHTML = gridHtml+= `
+//           <div>
+//           <span>${extension.name}</span>
+//           <img src= ${extension.logo}>
+//           <figcaption>${extension.description}</figcaption>
+//           </div>
+//           `
+//         })
+//     })    .catch(error=>{
+//         console.error('There was a problem', error)
+//     })

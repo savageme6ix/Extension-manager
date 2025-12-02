dcd75f91cddc86d919b544fb6f08acd34ce1931a
savageme6ix <maingithomas803@gmail.com>
2025-12-02 10:58:27 +0300

    Set grid

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-// let gridHtml = ``;
-// const extensionss = fetch("data.json")
-//     .then(response=>{
-//         if(!response.ok){
-//             throw new Error (`Http error! status: ${response.status}`);
-//         }
-//         return response.json();
-//     })
-//     .then(data=>{
-//         console.log(data)
-//         data.forEach((extension)=>{
-//             console.log(extension.name)
-//           const main = document.querySelector(".grid-container")
-//           main.innerHTML = gridHtml+= `
-//           <div>
-//           <span>${extension.name}</span>
-//           <img src= ${extension.logo}>
-//           <figcaption>${extension.description}</figcaption>
-//           </div>
-//           `
-//         })
-//     })    .catch(error=>{
-//         console.error('There was a problem', error)
-//     })
+let gridHtml = ``;
+const extensionss = fetch("data.json")
+    .then(response=>{
+        if(!response.ok){
+            throw new Error (`Http error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data=>{
+        console.log(data)
+        data.forEach((extension)=>{
+            console.log(extension.name)
+          const main = document.querySelector(".grid-container")
+          main.innerHTML = gridHtml+= `
+          <div class="ex-container">
+            <div class="wrap">
+            <img src = "${extension.logo}">
+            <div class="mini">
+            <span class="name">${extension.name}</span>
+            <figcaption class="cap">${extension.description}</figcaption>
+        </div> 
+        </div>
+        </div>
+          `
+        })
+    })    .catch(error=>{
+        console.error('There was a problem', error)
+    })
